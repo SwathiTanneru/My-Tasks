9c03263d03eeea3bac49a604ef514f80ace404d9
SwathiTanneru <156947543+SwathiTanneru@users.noreply.github.com>
2024-01-17 10:49:37 -0800

    Update Stringtasks.py

--- Stringtasks.py
+++ Stringtasks.py
@@ -1,5 +1,4 @@
-Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+
 >>> #1.Given a string, return the string made of its first two chars, so the String "Hello" yields "He". If the string is shorter than length 2, return whatever there is, so "X" yields "X", and the empty string "" yields the empty string "".
 >>> 
 >>> 
